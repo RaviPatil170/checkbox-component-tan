65ae4d965b465420f0bb0dbf584ba7eba6d5c1d9
Ravi Patil <captainkohli@Ravis-MacBook-Air.local>
2025-01-06 22:45:12 +0530

    initial commit for checkbox component

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// Write custom JavaScript here.
+// You may ignore this file and delete if if JavaScript is not required for your challenge.
+console.log("helo")
+const chk=document.getElementById('Indeterminate');
+chk.indeterminate=true;
+
+const chk1=document.getElementById('Checked');
+chk1.checked=true;
